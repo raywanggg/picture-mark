9575a357967481f34ac14ce9eac72f6fbdc5fe96
VieZhong <zyw13579@qq.com>
2017-07-24 16:46:09 +0800

    修复图像不能还原的bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 // 主入口文件
 import markShape from './markShape';
-import imgFilter from './imgFilter';
+import handleEffect from './imgFilter';
 import imgUrl from './image/test1.jpg';
 import './styles';
 
@@ -9,7 +9,7 @@ const control_panel = document.getElementById("control-panel");
 const drawing = document.getElementById("drawing");
 const cxt = drawing.getContext("2d");
 const e = window.event||e;
-const originData = cxt.getImageData(0, 0, 1150, 650);//获取初始图像data方便滤镜
+let originData;
 markShape.cxt = cxt;
 markShape.canvas = drawing;
 markShape.panel = control_panel;
@@ -47,16 +47,17 @@ document.addEventListener('mousemove', movePanel, false);
 
 //侧栏点击
 // 1、control_panel.click();
-const clickPanel = e => {
-	let type = e.target.getAttribute("data-shape-type");
+const clickPanel = ({target}) => {
+	const type = target.getAttribute("data-shape-type");
 	markShape[type]();
 	// type = "add" + type.charAt(0).toUpperCase() + type.toLowerCase().slice(1);//2、es6转换大小写
 	// method[type](markShape);
 };
 control_panel.addEventListener('click', clickPanel, false);
 
-const clickFilter = e => {
-	let type = e.target.getAttribute("data-filter-type");
-	imgFilter.handleEffect(type, cxt, originData);
+const clickFilter = ({target}) => {
+	const type = target.getAttribute("data-filter-type");
+    originData = originData || cxt.getImageData(0, 0, 1150, 650);//获取初始图像data方便滤镜
+	handleEffect(type, cxt, originData);
 }
 document.addEventListener('click', clickFilter, false);
